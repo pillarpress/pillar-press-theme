99a12185b8bebe1e264ea565a2353429848d7c1a
Robert DeVore <deviodigital@gmail.com>
2018-05-07 17:17:39 -0400

    :ok_hand: IMPROVE: Updated cleanblog names to pillar-press

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php
+pp-theme<?php
 /**
  * Pillar Press - Theme functions and definitions
  *
@@ -94,18 +94,18 @@ add_action( 'after_setup_theme', 'ppt_content_width', 0 );
  * Enqueue scripts and styles.
  */
 function ppt_scripts() {
-	wp_enqueue_style( 'cleanblog-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'cleanblog-bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
-	wp_enqueue_style( 'cleanblog-theme', get_template_directory_uri() . '/css/clean-blog.min.css' );
-	wp_enqueue_style( 'cleanblog-fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
-	wp_enqueue_style( 'cleanblog-lora', '//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' );
-	wp_enqueue_style( 'cleanblog-opensans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' );
+	wp_enqueue_style( 'pillar-press-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'pillar-press-bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+	wp_enqueue_style( 'pillar-press-theme', get_template_directory_uri() . '/css/pp-theme.min.css' );
+	wp_enqueue_style( 'pillar-press-fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'pillar-press-lora', '//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' );
+	wp_enqueue_style( 'pillar-press-opensans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' );
 
-	wp_enqueue_script( 'cleanblog-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20180318', true );
-	wp_enqueue_script( 'cleanblog-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20180318', true );
-	wp_enqueue_script( 'cleanblog-jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '20180318', true );
-	wp_enqueue_script( 'cleanblog-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '20180318', true );
-	wp_enqueue_script( 'cleanblog-theme', get_template_directory_uri() . '/js/clean-blog.min.js', array(), '20180318', true );
+	wp_enqueue_script( 'pillar-press-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20180507', true );
+	wp_enqueue_script( 'pillar-press-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20180507', true );
+	wp_enqueue_script( 'pillar-press-jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '20180507', true );
+	wp_enqueue_script( 'pillar-press-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '20180507', true );
+	wp_enqueue_script( 'pillar-press-theme', get_template_directory_uri() . '/js/pp-theme.min.js', array(), '20180507', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
