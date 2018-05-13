c31b845edfd283bac3edb1040165a841c54c4b91
Robert DeVore <deviodigital@gmail.com>
2018-05-12 23:12:10 -0400

    :ok_hand: IMPROVE: Remove unused code

--- functions.php
+++ functions.php
@@ -57,18 +57,6 @@ function ppt_setup() {
 		'caption',
 	) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	//	add_theme_support( 'post-formats', array(
-	//		'aside',
-	//		'image',
-	//		'video',
-	//		'quote',
-	//		'link',
-	//	) );
-
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'ppt_custom_background_args', array(
 		'default-color' => 'ffffff',
