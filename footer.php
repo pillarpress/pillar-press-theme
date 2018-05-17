57f5829f3555636fb10fb01a57bf692d518050a6
Robert DeVore <deviodigital@gmail.com>
2018-05-17 15:15:19 -0400

    :ok_hand: IMPROVE: This way, you must do it

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
   				<?php do_action( 'ppt_footer_top' ); ?>
   					<?php ppt_social(); ?>
-  				<?php if ( get_theme_mod( 'ppt_footer_copyright_text' ) !='' ) { ?>
+  				<?php if ( '' !== get_theme_mod( 'ppt_footer_copyright_text' ) ) { ?>
   					<p class="copyright text-muted"><?php echo get_theme_mod( 'ppt_footer_copyright_text' ); ?></p>
   				<?php } else { ?>
             <p class="copyright text-muted"><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pillar-press-theme' ) ); ?>" target="_blank"><?php printf( esc_html__( 'Powered by %s', 'pillar-press-theme' ), 'WordPress' ); ?></a> &middot; <?php printf( esc_html__( '%1$s by %2$s.', 'pillar-press-theme' ), 'Theme', '<a href="http://pillar.press" rel="designer" target="_blank">Pillar Press</a>' ); ?></p>
