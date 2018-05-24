6d67507a43edaa629852253937e824b0b07c0065
Robert DeVore <deviodigital@gmail.com>
2018-05-23 20:14:00 -0400

    :ok_hand: IMPROVE: Removed HTML5 support for IE8

--- header.php
+++ header.php
@@ -12,14 +12,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/respond.min.js"></script>
-    <![endif]-->
-
 <?php wp_head(); ?>
 </head>
 
