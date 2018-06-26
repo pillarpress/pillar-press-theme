28d8b6beae9e985411f122c8e22d3051d3e74360
Robert DeVore <deviodigital@gmail.com>
2018-06-26 15:43:16 -0400

    :ok_hand: IMPROVE: Removed header style function

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -27,47 +27,6 @@ function ppt_custom_header_setup() {
 }
 add_action( 'after_setup_theme', 'ppt_custom_header_setup' );
 
-if ( ! function_exists( 'ppt_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog
- *
- * @see ppt_custom_header_setup().
- */
-function ppt_header_style() {
-	$header_text_color = get_header_textcolor();
-
-	// If no custom options for text are set, let's bail
-	// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value.
-	if ( HEADER_TEXTCOLOR == $header_text_color ) {
-		return;
-	}
-
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( 'blank' == $header_text_color ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo esc_attr( $header_text_color ); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
-endif; // ppt_header_style
-
 if ( ! function_exists( 'ppt_admin_header_style' ) ) :
 /**
  * Styles the header image displayed on the Appearance > Header admin panel.
